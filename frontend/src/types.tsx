ecf27d5db7430d46b7483bd1f6ef38c3b4853123
lakhwinder <lakhwindersinghx2@gmail.com>
2025-08-02 16:17:15 -0400

    updated types.tsx

--- frontend/src/types.tsx
+++ frontend/src/types.tsx
@@ -1,7 +1,10 @@
+// This file defines shared types for your frontend and backend
 export interface TempProfile {
   account_id: string
   name: string
   city: string
   profession: string
   available: boolean
+  experience?: string
+  rate?: number
 }
